8b882994c2728e4447e4fea1de724b0976f069e3
Maria Sevostyanova <letanina.maria@gmail.com>
2020-04-02 11:42:22 +0400

    Test error

--- src/test/java/page/LoginPageV1.java
+++ src/test/java/page/LoginPageV1.java
@@ -10,8 +10,13 @@ public class LoginPageV1 {
         $("[data-test-id=login] input").setValue(info.getLogin());
         $("[data-test-id=password] input").setValue(info.getPassword());
         $("[data-test-id=action-login]").click();
+        $("[name=code]").setValue("12345");
         $("[data-test-id=action-verify]").click();
-       // $("[data-test-id=action-deposit]").click();
+        $("[data-test-id=action-deposit]").click();
+        $("[type=text]").setValue("200");
+        $("[type=tel]").setValue("5559 0000 0000 0002");
+        $("[data-test-id=action-transfer]").click();
         return new VerificationPage();
     }
+
 }
--- src/test/java/test/MoneyTransferTest.java
+++ src/test/java/test/MoneyTransferTest.java
@@ -3,32 +3,23 @@ package test;
 import data.DataHelper;
 import lombok.val;
 import org.junit.jupiter.api.Test;
+import page.DashboardPage;
 import page.LoginPageV1;
-//import page.LoginPageV2;
-//import page.LoginPageV3;
+
 
 
 import static com.codeborne.selenide.Selenide.open;
 
 class MoneyTransferTest {
+
+    private DashboardPage dashboardPage;
     @Test
     void shouldTransferMoneyBetweenOwnCardsV1() {
         open("http://localhost:9999");
         val loginPage = new LoginPageV1();
-       //   val loginPage = open("http://localhost:9999", LoginPageV1.class);
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
         val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+        dashboardPage = verificationPage.validVerify(verificationCode);
     }
-
-
-   // @Test
-   // void shouldTransferMoneyBetweenOwnCardsV3() {
-    //    val loginPage = open("http://localhost:9999", LoginPageV3.class);
-    //    val authInfo = DataHelper.getAuthInfo();
-   //     val verificationPage = loginPage.validLogin(authInfo);
-    //    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-   //     verificationPage.validVerify(verificationCode);
-  //  }
 }
